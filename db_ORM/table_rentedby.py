78c8f7b0361543240ba9eb6ab929e38bd5d06282
Lorenzo <lorenzo.amata@mail.polimi.it>
2020-11-09 22:26:29 +0100

    eliminated single files for tables due to circular depend.

--- db_ORM/table_rentedby.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine,Table,Column,Integer,String,TIMESTAMP,ForeignKey,Float,ForeignKeyConstraint,MetaData,Date,CheckConstraint
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from database import Base
-
-Rentedby = Table('Rentedby',Base.metadata,
-    Column('Idrentalcontract',String(20),ForeignKey('Rentalcontract.Idrentalcontract'),nullable=False),
-    Column('ClientSSN',String(9),ForeignKey('Clients.SSN'),nullable=False)
-    )
